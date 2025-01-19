0787ec533d53ee16eab088fe18bccd52822c5f8d
ipritom <pritom.blue2@gmail.com>
2025-01-19 22:10:07 +0600

    new: reading file

---
+++ reading-poetry/readme.md
@@ -0,0 +1,9 @@
+# Reading Poetry
+
+Example code to read text file and print it out.
+
+Also if you want to see the documentation of the helper function run the following command.
+
+```bash
+cargo doc --open
+```
---
+++ reading-poetry/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+/// Helper function to get the path of a file in a project directory (`cargo run` compatible)
+/// 
+/// Example: 
+/// ```
+/// let file_path : String = collect_path(r"data\poem.txt".to_string());
+/// ```
+fn collect_path(file_name:String)->String{
+    let mut current_dir: String =  std::env::current_dir().unwrap().display().to_string();
+    current_dir.push_str(r"\");
+    current_dir.push_str(file_name.trim());
+    current_dir
+
+}
+/// asdasd
+fn main() {
+    let file_path : String = collect_path(r"data\poem.txt".to_string());
+    println!("File Path: {file_path}\n");
+    let content = fs::read_to_string(file_path).expect("Should have been able to read file");
+    println!("{content}");
+}
