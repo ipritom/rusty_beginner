c32c33a3dbd336725c3938c2e2b2406f8d805d8a
ipritom <pritom.blue2@gmail.com>
2025-01-19 22:12:44 +0600

    minor: code clean and add comment

--- reading-poetry/src/main.rs
+++ reading-poetry/src/main.rs
@@ -11,12 +11,14 @@ fn collect_path(file_name:String)->String{
     current_dir.push_str(r"\");
     current_dir.push_str(file_name.trim());
     current_dir
-
 }
-/// asdasd
+
+
 fn main() {
+    // declare and set file_path
     let file_path : String = collect_path(r"data\poem.txt".to_string());
     println!("File Path: {file_path}\n");
+    // read the text file
     let content = fs::read_to_string(file_path).expect("Should have been able to read file");
     println!("{content}");
 }
